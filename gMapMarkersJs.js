8a8b0fac0adf13c980267d0d5f1ef6ec23a022eb
Elisardo Felix <elisardofelix@gmail.com>
2016-11-22 16:49:04 -0400

    Change the callback flow
     - Now the object firts set the configuration then select the markers and at last initialize the show process of the map with the google maps api.

--- gMapMarkersJs.js
+++ gMapMarkersJs.js
@@ -1,61 +1,71 @@
-var mapsmarker = function (key) {
+function mapsmarker(idmap, mapzoon, maptype) {
 
-				jQuery(function($) {
-			    // Asynchronously Load the map API 
-			    var script = document.createElement('script');
-			    script.src = "//maps.googleapis.com/maps/api/js?sensor=false&callback=initialize&key="+key;
-			    document.body.appendChild(script);
-			    });
-
-				//Initialize the markers data container
-				this.markers = {};
-
-				return (function () {
-								    var map;
-								    var bounds = new google.maps.LatLngBounds();
-								    var mapOptions = {
-								        mapTypeId: 'roadmap'
-								    }});
+        //Initialize the markers data container 
+        this.idmap = idmap;
+        this.mapzoon = mapzoon;
+        this.maptype = maptype;
 };
 
-mapsmarker.prototype.addMarker = function(plon, plat, ptitle, ptext){
-			this.markers.push({lon : plon, lat : plat, title : ptitle, text : ptext});
+
+mapsmarker.prototype.addMarker = function (plon, plat, ptitle, ptext){
+
+      if(jQuery.isEmptyObject(this.markers))
+        this.markers = [{lon : plon, lat : plat, title : ptitle, text : ptext}];
+      else
+        this.markers.push({lon : plon, lat : plat, title : ptitle, text : ptext});
 };
 
-mapsmarker.prototype.showMap = function () {
-			// Display a map on the page
-		    map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
-		    map.setTilt(45);
-		        
-		    // Display multiple markers on a map
-		    var infoWindow = new google.maps.InfoWindow(), marker, i;
-		    
-		    // Loop through our array of markers & place each one on the map  
-		    for( i = 0; i < this.markers.length; i++ ) {
-		        var position = new google.maps.LatLng(this.markers[i].lon, this.markers[i].lat);
-		        bounds.extend(position);
-		        marker = new google.maps.Marker({
-		            position: position,
-		            map: map,
-		            title: this.markers[i].title
-		        });
-		        
-		        // Allow each marker to have an info window    
-		        google.maps.event.addListener(marker, 'click', (function(marker, i) {
-		            return function() {
-		                infoWindow.setContent(this.markers[i].text);
-		                infoWindow.open(map, marker);
-		            }
-		        })(marker, i));
+mapsmarker.prototype.initialize = function (key, markers = this.markers, idmap = this.idmap, mapzoon = this.mapzoon, maptype = this.maptype) {
 
-		        // Automatically center the map fitting all markers on the screen
-		        map.fitBounds(bounds);
-		    }
+  jQuery(function($) {
+      // Asynchronously Load the map API 
+      var script = document.createElement('script');
+      script.src = "//maps.googleapis.com/maps/api/js?sensor=false&callback=initialize&key="+key;
+      document.body.appendChild(script);
+      });
 
-		    // Override our map zoom level once our fitBounds function runs (Make sure it only runs once)
-		    var boundsListener = google.maps.event.addListener((map), 'bounds_changed', function(event) {
-		        this.setZoom(14);
-		        google.maps.event.removeListener(boundsListener);
-		    });
+    return function () {
+                          var map;
+                          var bounds = new google.maps.LatLngBounds();
+                          var mapOptions = {
+                              mapTypeId: maptype
+                          };
+
+                        // Display a map on the page
+                          map = new google.maps.Map(document.getElementById(idmap), mapOptions);
+                          map.setTilt(45);
+                              
+                          // Display multiple markers on a map
+                          var infoWindow = new google.maps.InfoWindow(), marker, i;
+                          
+                          // Loop through our array of markers & place each one on the map  
+                          for( i = 0;markers[i]; i++ ) {
+                              var position = new google.maps.LatLng(markers[i].lon, markers[i].lat);
+                              bounds.extend(position);
+                              marker = new google.maps.Marker({
+                                  position: position,
+                                  map: map,
+                                  title: markers[i].title
+                              });
+                              
+                              // Allow each marker to have an info window    
+                              google.maps.event.addListener(marker, 'click', (function(marker, i) {
+                                  return function() {
+                                      infoWindow.setContent(markers[i].text);
+                                      infoWindow.open(map, marker);
+                                  }
+                              })(marker, i));
+
+                              // Automatically center the map fitting all markers on the screen
+                              map.fitBounds(bounds);
+                          }
+
+                          // Override our map zoom level once our fitBounds function runs (Make sure it only runs once)
+                          var boundsListener = google.maps.event.addListener((map), 'bounds_changed', function(event) {
+                              this.setZoom(mapzoon);
+                              google.maps.event.removeListener(boundsListener);
+                          });
+
+                  };
 
 };
