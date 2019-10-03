15d125c8231f31eae45adb18582beea62f540bed
Elisardo Felix <elisardofelix@gmail.com>
2019-10-03 01:08:52 -0400

    Final changes

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-let jQuery = require("jquery");
+const jQuery = require("jquery");
 
 let Gmarkers = [];
+let $this = { markers: [], initialized: false };
 
-let mapsmarker = (
+let mapsmarker = function(
   idmap,
   mapzoom,
   maptype,
@@ -10,35 +11,26 @@ let mapsmarker = (
   markers = [],
   selectable = false,
   pcenter = { lat: 18.48117202, lng: -69.92138672 }
-) => {
-
+) {
   //Initialize the markers data container
-  this.idmap = idmap;
-  this.mapzoom = mapzoom;
-  this.maptype = maptype;
-  this.pcenter = pcenter;
-  this.selectable = selectable;
-  this.markers = [];
-
-  if (markers.length > 0) mapsmarker.initialize(key, markers);
+  $this.idmap = idmap;
+  $this.mapzoom = mapzoom;
+  $this.maptype = maptype;
+  $this.pcenter = pcenter;
+  $this.selectable = selectable;
+  $this.markers = [];
+  initialize(key, markers);
 };
 
-mapsmarker.prototype.addMarker = (plat, plon, ptitle, ptext) => {
-  this.markers.push({ lat: plat, lon: plon, title: ptitle, text: ptext });
+mapsmarker.prototype.addMarker = function(plat, plon, ptitle, ptext) {
+  if ($this.initialized)
+    setMarker({ lat: plat, lon: plon, title: ptitle, text: ptext });
+  else $this.markers.push({ lat: plat, lon: plon, title: ptitle, text: ptext });
 };
 
-mapsmarker.prototype.markerFunction = fun => {
-  this.fun = fun;
-};
-
-mapsmarker.prototype.initialize = (key, pmarkers) => {
-  key = typeof key !== "undefined" ? key : "";
-  pmarkers = typeof pmarkers !== "undefined" ? pmarkers : [];
-
+const initialize = function(key = "", pmarkers = []) {
   //Only one parameter and better implementation.
-  if (typeof this.fun === "undefined")
-    let fun = () => { };
-  else let fun = this.fun;
+  if (pmarkers.length > 0) $this.markers = pmarkers;
 
   jQuery(() => {
     // Asynchronously Load the map API
@@ -49,81 +41,58 @@ mapsmarker.prototype.initialize = (key, pmarkers) => {
     document.body.appendChild(script);
   });
 
-  global.initMap = function () {
-    let map;
-    let bounds = new google.maps.LatLngBounds();
+  global.initMap = function() {
+    $this.initialized = true;
+    $this.bounds = new google.maps.LatLngBounds();
     let mapOptions = {
-      zoom: this.mapzoom,
-      mapTypeId: this.maptype,
-      center: { lat: this.pcenter.lat, lng: this.pcenter.lng }
+      zoom: $this.mapzoom,
+      mapTypeId: $this.maptype,
+      center: { lat: $this.pcenter.lat, lng: $this.pcenter.lng }
     };
 
     // Display a map on the page
-    map = new google.maps.Map(document.getElementById(this.idmap), mapOptions);
-    map.setTilt(45);
+    $this.map = new google.maps.Map(
+      document.getElementById($this.idmap),
+      mapOptions
+    );
+    $this.map.setTilt(45);
 
     // Display multiple markers on a map
-    let infoWindow = new google.maps.InfoWindow(),
-      marker,
-      i;
-    let until = -1;
-
-    if (this.selectable && jQuery.isEmptyObject(this.markers) == false) until = 1;
-    else if (!this.selectable) until = this.markers.length;
+    $this.infoWindow = new google.maps.InfoWindow();
 
     // Loop through our array of markers & place each one on the map
-    for (i = 0; i < until; i++) {
-      let position = new google.maps.LatLng(this.markers[i].lat, this.markers[i].lon);
-      bounds.extend(position);
-      marker = new google.maps.Marker({
-        position: position,
-        map: map,
-        title: this.markers[i].title
-      });
-
-      // Allow each marker to have an info window
-      google.maps.event.addListener(
-        marker,
-        "click",
-        (function (marker, i) {
-          return function () {
-            infoWindow.setContent(this.markers[i].text);
-            infoWindow.open(map, marker);
-          };
-        })(marker, i)
-      );
-
-      // Automatically center the map fitting all markers on the screen
-
-      map.fitBounds(bounds);
-      Gmarkers.push(marker);
-    }
+    for (let i = 0; i < $this.markers.length; i++) setMarker($this.markers[i]);
 
     // Override our map zoom level once our fitBounds function runs (Make sure it only runs once)
     let boundsListener = google.maps.event.addListener(
-      map,
+      $this.map,
       "bounds_changed",
-      event => {
-        this.setZoom(this.mapzoom);
+      function(event) {
+        this.setZoom($this.mapzoom);
         google.maps.event.removeListener(boundsListener);
       }
     );
-
-
-    if (this.selectable) {
-      google.maps.event.addListener(map, "click", event => {
-        if (Gmarkers.length == 0) {
-          marker = new google.maps.Marker({
-            position: event.latLng,
-            map: map
-          });
-          Gmarkers.push(marker);
-        } else Gmarkers[0].setPosition(event.latLng);
-
-        fun();
-      });
-    }
   };
+};
+
+const setMarker = function(pmarker) {
+  let position = new google.maps.LatLng(pmarker.lat, pmarker.lon);
+  $this.bounds.extend(position);
+  let marker = new google.maps.Marker({
+    position: position,
+    map: $this.map,
+    title: pmarker.title
+  });
+
+  // Allow each marker to have an info window
+  google.maps.event.addListener(marker, "click", function() {
+    $this.infoWindow.setContent(pmarker.text);
+    $this.infoWindow.open($this.map, marker);
+  });
+
+  // Automatically center the map fitting all markers on the screen
+  $this.map.fitBounds($this.bounds);
+  Gmarkers.push(marker);
 };
 
 module.exports = mapsmarker;
